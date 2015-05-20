1d6e04910c3a06c856e81f5c8efefa4ecf3414d3
Robert <robert.schoefbeck@cern.ch>
2015-05-20 13:08:24 +0200

    Simplifications in helpers

--- simplePlot/python/helpers.py
+++ simplePlot/python/helpers.py
@@ -2,58 +2,34 @@ import ROOT
 from math import pi, sqrt, cos, sin, sinh, log
 from array import array
 
-def getFileList(dir, minAgeDPM=0, histname='histo', xrootPrefix='root://hephyse.oeaw.ac.at/', maxN=-1):
-  import os, subprocess, datetime
-  monthConv = {'Jan':1, 'Feb':2,'Mar':3,'Apr':4,"May":5, "Jun":6,"Jul":7,"Aug":8, "Sep":9, "Oct":10, "Nov":11, "Dec":12}
-  if dir[0:5] != "/dpm/":
-    filelist = os.listdir(os.path.expanduser(dir))
-    filelist = [dir+'/'+f for f in filelist if histname in f]
-  else:
-    filelist = []
-    p = subprocess.Popen(["dpns-ls -l "+ dir], shell = True , stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    for line in p.stdout.readlines():
-      if not (histname=="" or line.count(histname)):continue
-      line=line[:-1]
-      sline = line.split()
-      fname = sline[-1]
-      size = sline[4]
-      if int(size)!=0:
-        month, day = sline[5:7]
-        hour, minute = sline[7].split(':')
-        age = (datetime.datetime.now() - datetime.datetime(2014, monthConv[month], int(day), int(hour), int(minute))).total_seconds()/3600
-        if age>=minAgeDPM:
-          filelist.append(fname)
-        else:
-          print "Omitting",fname,'too young:',str(age)+'h'
-    filelist = [xrootPrefix+dir+'/'+f for f in filelist]
+def getFileList(dir, histname='histo', maxN=-1):
+  import os
+  filelist = os.listdir(os.path.expanduser(dir))
+  filelist = [dir+'/'+f for f in filelist if histname in f]
   if maxN>=0:
     filelist = filelist[:maxN]
   return filelist
 
-def getChain(sL, minAgeDPM=0, histname='histo', xrootPrefix='root://hephyse.oeaw.ac.at/', maxN=-1, treeName="Events"):
-  if not type(sL)==type([]):
-    sList = [sL]
+def getChain(sampleList, histname='histo', maxN=-1, treeName="Events"):
+  if not type(sampleList)==type([]):
+    sampleList_ = [sampleList]
   else:
-    sList= sL 
+    sampleList_= sampleList 
   c = ROOT.TChain(treeName)
   i=0
-  for s in sList:
+  for s in sampleList_:
     if type(s)==type(""):
-      for f in getFileList(s, minAgeDPM, histname, xrootPrefix, maxN):
+      for f in getFileList(s, histname, maxN):
         i+=1
         c.Add(f)
     if type(s)==type({}):
       if s.has_key('file'):
         c.Add(s['file'])
         i+=1
-      if s.has_key('fromDPM') and s['fromDPM']:
-        for f in getFileList(s['dir'], minAgeDPM, histname, xrootPrefix, maxN):
-          i+=1
-          c.Add(f)
       if s.has_key('bins'):
         for b in s['bins']:
           dir = s['dirname'] if s.has_key('dirname') else s['dir']
-          for f in getFileList(dir+'/'+b, minAgeDPM, histname, xrootPrefix, maxN):
+          for f in getFileList(dir+'/'+b, histname, maxN):
             i+=1
             c.Add(f)
   print "Added ",i,'files from sample',s['name']
