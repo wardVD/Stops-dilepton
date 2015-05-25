7d226cdc458f782f128637f7b39f76bc5e218011
Administrator <wardvandriessche@gmail.com>
2015-05-25 15:47:45 +0200

    update localInfo

--- tools/python/localInfo.py
+++ tools/python/localInfo.py
@@ -1,5 +1,5 @@
 import os
-if os.environ['USER'] in ['ward']:
+if os.environ['USER'] in ['wvandrie']:
   plotDir = "/afs/cern.ch/user/w/wvandrie/www/Stops/"
   dataDir = "~/eos/cms/store/cmst3/group/susy/schoef/postProcessed_Phys14V3_diLep" #needs EOS mount on lxplus at ~/eos
 if os.environ['USER'] in ['didar']:
