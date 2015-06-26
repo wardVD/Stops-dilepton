b65dfa7ca71ad0d90b8bf3d1454ec7b42f35f1e1
Administrator <sigamani@cern.ch>
2015-06-26 21:47:22 +0200

    Adding TMVA directory

---
+++ TMVA/test/AutoDict_vector_TControlBar__.cxx
@@ -0,0 +1,13 @@
+#include "vector"
+class TControlBar;
+#ifdef __CINT__ 
+#pragma link C++ nestedclasses;
+#pragma link C++ nestedtypedefs;
+#pragma link C++ class vector<TControlBar*>+;
+#pragma link C++ class vector<TControlBar*>::*;
+#ifdef G__VECTOR_HAS_CLASS_ITERATOR
+#pragma link C++ operators vector<TControlBar*>::iterator;
+#pragma link C++ operators vector<TControlBar*>::const_iterator;
+#pragma link C++ operators vector<TControlBar*>::reverse_iterator;
+#endif
+#endif
